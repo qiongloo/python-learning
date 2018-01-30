938b08ffe02c8aef943afdff9797daad23a60142
yangfeng <e@qiongloo.com>
2018-01-30 10:28:51 +0800

    第一次书写

---
+++ Lxf/01fisrt-process/first.py
@@ -0,0 +1,20 @@
+#输入和输出
+
+#打印
+print('hello world')
+
+#,号隔开的话 结果是空格隔开
+print('the quick brown fox','jumps over','the lazy dog')
+
+#数字打印
+print(300)
+print(100+200)
+print('100 + 200 =',100+200)
+
+#输入 input()接收键盘输入
+#name = input()
+# name = input('please enter your name: ')
+# print('hello,',name)
+
+#### 练习
+print('1024 * 768 =',1024*768)
